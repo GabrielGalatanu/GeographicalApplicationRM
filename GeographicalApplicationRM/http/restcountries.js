eea19088c84c1cb1fb7ffc9e12fab80a2dcc56e8
Gabi Galatanu <mihai.gabriel.galatanu@gmail.com>
2021-10-06 09:10:47 +0300

    Http

---
+++ GeographicalApplicationRM/http/restcountries.js
@@ -0,0 +1,38 @@
+
+/**
+ * Returns all data about a country.
+ * 
+ * @param {string} country - Country name.
+ * @returns {Object}
+ */
+export const getCountryAPI = async (country) => {
+  try {
+    const response = await fetch(`https://restcountries.com/v2/name/${country}`);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Returns all countries from a region.
+ * 
+ * @param {string} region - Region name. (Europe, Africa, Americas, Asia, Oceania)
+ * @returns {Object}
+ */
+
+export const getAllCountriesByRegionAPI = async (region) => {
+  
+    try {
+      const response = await fetch(
+        `https://restcountries.com/v3.1/region/${region}`,
+      );
+      const json = await response.json();
+
+      return json;
+    } catch (error) {
+      console.error(error);
+    }
+  
+};
--- GeographicalApplicationRM/screens/study/RegionListScreen.js
+++ GeographicalApplicationRM/screens/study/RegionListScreen.js
@@ -1,29 +1,34 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {Button, StyleSheet, Text, View} from 'react-native';
 
-// Import vector icons
-import Icon from 'react-native-vector-icons/Ionicons';
-
 const RegionListScreen = props => {
+  
+  useEffect(() => {
+    // (async () => {
+    //   setCountries(await getCountryAPI('romania'));
+    // })();
+
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text> RegionListScreen </Text>
-      <Button title="Go to Country List" onPress={() => {
-        props.navigation.navigate('CountriesListScreen');
-      }}/>
-       <Icon name="game-controller" size={30} color="#900" />
+      <Button
+        title="Go to Country List"
+        onPress={() => {
+          props.navigation.navigate('CountriesListScreen');
+        }}
+      />
     </View>
   );
 };
 
 export const screenOptions = () => {
-    return {
-      headerTitle: 'Regions!',
-    };
+  return {
+    headerTitle: 'Regions!',
   };
+};
 
 const styles = StyleSheet.create({
   container: {
